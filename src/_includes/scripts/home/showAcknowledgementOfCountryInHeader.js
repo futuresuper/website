8acdfe140fe038c45b95c4525e661d72e02dd732
Danny White <hi@dannywhite.site>
2021-01-06 17:12:14 +1000

    feat: improved styling and text

--- src/_includes/scripts/home/showAcknowledgementOfCountryInHeader.js
+++ src/_includes/scripts/home/showAcknowledgementOfCountryInHeader.js
@@ -2,15 +2,15 @@ const aocHeader = document.querySelector(".acknowledgement-of-country.header");
 // siteContent already declared in global.js
 
 // Local storage
-const alreadyVisited = localStorage.getItem("alreadyVisited");
+const visited = localStorage.getItem("visited");
 // If visiting for the first time...
-if (!alreadyVisited) {
+if (!visited) {
   // Show acknowledgement of country in the header
   aocHeader.classList.add("active");
   // Tell siteContent to make room for it
   siteContent.classList.add("aoc-active");
   // Prepare the body so the mobile menu also makes room for it
   document.body.classList.add("aoc-active");
-  // Then set alreadyVisited to true so it doesn't show next time
-  localStorage.setItem("alreadyVisited", true);
+  // Then set visited to true so it doesn't show next time
+  localStorage.setItem("visited", true);
 }
