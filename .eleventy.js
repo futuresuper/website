1b5f0cec911cec0e5cab7248f9d6ad50b8698fa8
Andrew Sellen <andrewmsellen@gmail.com>
2021-01-27 10:29:52 +1100

    Added hash sumbol to slugify chars to strip

--- .eleventy.js
+++ .eleventy.js
@@ -91,11 +91,11 @@ module.exports = (config) => {
   });
 
   // Override default 'Slug' filter with below options required to work properly
-  // https://github.com/11ty/eleventy/issues/278#issuecomment-453777880
+  // https://github.com/11ty/eleventy/issues/278#issuecomment-451105828
   config.addFilter("slug", (input) => {
     const options = {
       replacement: "-",
-      remove: /[&,+()$~%.'":*?<>{}]/g,
+      remove: /[&,+()$~%.'":*?<>{}#]/g,
       lower: true,
     };
     return slugify(input, options);
