34b18f045e152e8a31f3e6c830debf690118341f
Danny White <danny@futuresuper.com.au>
2021-02-08 17:21:57 +1100

    feat: starburst animation for review stars

--- src/_includes/scripts/home/productReviewCarousel2.js
+++ src/_includes/scripts/home/productReviewCarousel2.js
@@ -3,7 +3,7 @@ new Glide(".glide", {
   autoplay: 4000,
   animationDuration: 600,
   // *Larger* than $breakpoint-lg
-  gap: 96, // px between each item
+  gap: 128, // px between each item
   peek: 56, // px on side margins
   perView: 2,
   breakpoints: {
