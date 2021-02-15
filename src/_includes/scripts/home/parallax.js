692710729d20a6d44b35f905f36481c83a6c7172
Danny White <danny@futuresuper.com.au>
2021-02-15 15:42:19 +1100

    feat: updated coal sprite

--- src/_includes/scripts/home/parallax.js
+++ src/_includes/scripts/home/parallax.js
@@ -1,10 +1,10 @@
-const rellaxPoster = new Rellax(".rellax-poster", {
+const rellaxForeground = new Rellax(".poster.rellax, .glide.rellax", {
   center: true,
   round: true,
   speed: 3,
   horizontal: false,
 });
-const rellaxImg = new Rellax(".rellax-img", {
+const rellaxMiddleDistance = new Rellax(".sprite.rellax", {
   center: true,
   round: true,
   speed: 0.5,
