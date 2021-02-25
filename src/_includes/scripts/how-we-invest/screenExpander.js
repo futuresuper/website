f3f764d8e99b12956580a2f22179c9182e52a814
Danny White <danny@futuresuper.com.au>
2021-02-25 12:11:00 +1100

    feat: removed two-way button for simplicity

--- src/_includes/scripts/how-we-invest/screenExpander.js
+++ src/_includes/scripts/how-we-invest/screenExpander.js
@@ -1,48 +1,11 @@
 const screensContainer = document.querySelector(".screens-container");
 const expanderButton = document.querySelector("button.expander");
 const screens = document.querySelector(".screens");
-let screensClosed = true;
 
 expanderButton.onclick = function () {
-  // If the screens container is closed/collapsed...
-  console.log("screensClosed is", screensClosed);
-
-  if (screensClosed) {
-    console.log("the full height is...", screens.clientHeight);
-    // Open screens container
-    screens.style.paddingBottom = `calc(${expanderButton.clientHeight}px + 2rem)`;
-    console.log("the full height with padding is...", screens.clientHeight);
-    screensContainer.style.maxHeight = `${screens.clientHeight}px`;
-
-    console.log("the end height is...", screens.clientHeight);
-    // console.log(screensContainer.style.height);
-    // screensContainer.classList.add("active");
-    expanderButton.classList.add("active");
-    // Add some extra space at the bottom to accommodate for the expander button
-    // screensContainer.style.paddingBottom = "4rem";
-
-    // Update the button text
-    expanderButton.textContent = "Show less";
-    // Update the variable for next time click
-    screensClosed = false;
-    // If the screens container is open...
-  } else {
-    // Close screens container
-    screens.style.paddingBottom = "inherit";
-    screensContainer.style.maxHeight = "64vh";
-    // console.log(screensContainer.style.height);
-    // screensContainer.classList.remove("active");
-
-    // screensContainer.onanimationend = () => {
-    expanderButton.classList.remove("active");
-    //   screensContainer.style.paddingBottom = "0";
-    // };
-
-    // Update the button text
-    expanderButton.textContent = "Show more";
-
-    // Update the variable for next time click
-
-    screensClosed = true;
-  }
+  // Open screens container
+  // Set to same height as its child contents
+  screensContainer.style.maxHeight = `${screens.clientHeight}px`;
+  // Hide expander button
+  expanderButton.classList.add("active");
 };
