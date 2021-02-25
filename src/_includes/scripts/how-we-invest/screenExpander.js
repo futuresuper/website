a648703568e45957bbc5c38b36c66bad3a93e9e1
Danny White <danny@futuresuper.com.au>
2021-02-25 12:47:01 +1100

    feat: simplified expander for better experience

--- src/_includes/scripts/how-we-invest/screenExpander.js
+++ src/_includes/scripts/how-we-invest/screenExpander.js
@@ -2,10 +2,36 @@ const screensContainer = document.querySelector(".screens-container");
 const expanderButton = document.querySelector("button.expander");
 const screens = document.querySelector(".screens");
 
-expanderButton.onclick = function () {
+let screensContainerExpanded = false;
+
+// Create function for handling screen container expansion from its minified state
+function expandScreenContainer() {
   // Open screens container
   // Set to same height as its child contents
-  screensContainer.style.maxHeight = `${screens.clientHeight}px`;
+  // screensContainer.style.maxHeight = `${screens.offsetHeight}px`;
+  screensContainer.classList.add("active");
   // Hide expander button
   expanderButton.classList.add("active");
-};
+
+  // Update variable
+  screensContainerExpanded = true;
+}
+
+// Call this function when the expander button is clicked
+expanderButton.onclick = expandScreenContainer;
+
+// Handle criteria switching
+function showScreen(screen) {
+  // Switch criteria
+  const screenContainers = document.getElementsByClassName("criteria-container");
+  for (let i = 0; i < screenContainers.length; i++) {
+    screenContainers[i].style.display = "none";
+  }
+  document.getElementById(screen).style.display = "block";
+
+  // If it's not already...
+  if (!screensContainerExpanded) {
+    // Automatically expand screen container
+    expandScreenContainer();
+  }
+}
