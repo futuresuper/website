83208f64dbc22669b23eb7c952a69301974cdf38
Danny White <danny@futuresuper.com.au>
2021-02-25 15:12:29 +1100

    feat: fully working criteria

--- src/_includes/scripts/how-we-invest/screenHandler.js
+++ src/_includes/scripts/how-we-invest/screenHandler.js
@@ -1,18 +1,17 @@
 const screensContainer = document.querySelector(".screens-container");
 const expanderButton = document.querySelector("button.expander");
 const screens = document.querySelector(".screens");
+const allCriteriaContainers = document.querySelectorAll(".criteria-container");
+const screensTooltipContainer = document.querySelector(".perma-tooltip-container");
 
 let screensContainerExpanded = false;
 
 // Create function for handling screen container expansion from its minified state
 function expandScreenContainer() {
   // Open screens container
-  // Set to same height as its child contents
-  // screensContainer.style.maxHeight = `${screens.offsetHeight}px`;
   screensContainer.classList.add("active");
   // Hide expander button
   expanderButton.classList.add("active");
-
   // Update variable
   screensContainerExpanded = true;
 }
@@ -22,13 +21,18 @@ expanderButton.onclick = expandScreenContainer;
 
 // Handle criteria switching
 function showScreen(screen) {
-  // Switch criteria
-  const screenContainers = document.getElementsByClassName("criteria-container");
-  for (let i = 0; i < screenContainers.length; i++) {
-    screenContainers[i].style.display = "none";
-  }
-  document.getElementById(screen).style.display = "block";
-
+  allCriteriaContainers.forEach((element) => {
+    // If this element matches the one clicked...
+    if (element.id === screen) {
+      // Make selected screen active
+      element.classList.add("active");
+    } else {
+      // And turn the rest off
+      element.classList.remove("active");
+    }
+  });
+  // Hide tooltip now that visitor has interacted
+  screensTooltipContainer.classList.add("active");
   // If it's not already...
   if (!screensContainerExpanded) {
     // Automatically expand screen container
